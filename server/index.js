a30673ad9bac012ed39e4dfde93c945998020ebf
Rahul Balhara <rahulbalhara821@gmail.com>
2026-07-21 20:49:09 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 //conectivity between frontend and backend
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://examnotes-generator-1.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
